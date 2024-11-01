c44cf302c49c3b1abcca402bb7dbaee7c6fa15e5
payn873 <142923202+payn873@users.noreply.github.com>
2024-11-01 14:51:07 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 4;
+
+class Matrix{
+    private:
+        int data[SIZE][SIZE];
+    
+    public:
+        void readFromStdin();
+        //void display() const;
+        //Matrix operator+(const Matrix& other) const;
+        //int sumOfDiagonals() const;
+        //void swapRows(int row1, int row2);
+};
+
+void Matrix::readFromStdin(){
+    //atoi(char arr)
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
+            cin >> data[i][j];
+        }
+    }
+}
+
+int main(){
+    Matrix m1;
+    Matrix m2;
+    cout << "Enter numbers for matrix 1: " << endl;
+    m1.readFromStdin();
+    cout << "Enter numbers for matrix 2: " << endl;
+    m2.readFromStdin();
+}
